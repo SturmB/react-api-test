785a35f21219f0037b4378732e0d3a02464747bb
Chris McGee <chris@chrismcgee.info>
2021-06-09 14:26:36 -0400

    ✨ Initial work to get the data displayed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,96 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+export function splitArray(array) {
+  const newArray = [];
+  array.forEach((str) => {
+    const regex = /\d{6}/;
+    const index = str.search(regex);
+    if (index === -1) {
+      newArray.push({ name: str });
+      return;
+    }
+    const storeName = str.slice(0, index - 1);
+    const date = str.match(regex)[0];
+    if (!newArray.find((obj) => obj.name === storeName)) {
+      const len = newArray.push({ name: storeName });
+      newArray[len - 1][`months`] = [];
+    }
+    newArray.find((obj) => obj.name === storeName)[`months`].push(date);
+  });
+  return newArray;
+}
+
+export function properName(str) {
+  const array = str.split(`_`);
+  const capitalizedArray = array.map(
+    (word) => word[0].toUpperCase() + word.substr(1)
+  );
+  return capitalizedArray.join(` `);
+}
+
+export function convertMonth(str) {
+  const monthNum = parseInt(str.slice(4, 6), 10);
+  const date = new Date();
+  date.setMonth(monthNum - 1);
+  return date.toLocaleString(`default`, { month: `short` });
+}
+
+export function convertMonths(arr) {
+  return arr.map((date) => convertMonth(date));
+}
+
+export function TableHead({ headers }) {
+  return (
+    <thead>
+      <tr>
+        <th rowSpan={2}>{headers[0].name}</th>
+        {headers.map((store) => {
+          if (store.hasOwnProperty(`months`)) {
+            return (
+              <th key={store.name} colSpan={Object.keys(store.months).length}>
+                {store.name}
+              </th>
+            );
+          }
+          return null;
+        })}
+        <th rowSpan={2}>{headers[headers.length - 1].name}</th>
+      </tr>
+      <tr>
+        {headers.map((store) => {
+          if (store.hasOwnProperty(`months`)) {
+            // noinspection JSUnresolvedVariable
+            return store.months.map((month) => {
+              return <th key={store.name + `-` + month}>{month}</th>;
+            });
+          }
+          return null;
+        })}
+      </tr>
+    </thead>
+  );
+}
+
+export function TableBody({ data, order }) {
+  return (
+    <tbody>
+      {data.map((row) => {
+        // noinspection JSUnresolvedVariable
+        return (
+          <tr key={row.brand}>
+            <td>{row.brand}</td>
+            {order.map((item) => {
+              return <td key={row.brand + item}>{row[item]}</td>;
+            })}
+            <td>{row.total}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+}
+
 function App() {
   const [allBrands, setAllBrands] = useState(null);
 
@@ -12,7 +102,37 @@ function App() {
   }, []);
 
   if (allBrands) {
-    return <div>{JSON.stringify(allBrands.data)}</div>;
+    console.log(allBrands.data[0]);
+    const columnsRaw = Object.entries(allBrands.data[0])
+      .map((point) => {
+        if (!/percent/.test(point[0])) {
+          return point[0];
+        }
+        return null;
+      })
+      .filter((point) => point !== null);
+    const dataOrder = [];
+    columnsRaw.forEach((column) => {
+      if (/\d{6}/.test(column)) {
+        dataOrder.push(column);
+      }
+    });
+    console.log(dataOrder);
+    const structured = splitArray(columnsRaw);
+    const capitalized = structured.map((obj) => {
+      obj.name = properName(obj.name);
+      if (obj.months) {
+        obj.months = convertMonths(obj.months);
+      }
+      return obj;
+    });
+    console.log(capitalized);
+    return (
+      <table>
+        <TableHead headers={capitalized} />
+        <TableBody data={allBrands.data} order={dataOrder} />
+      </table>
+    );
   }
 
   return <div>No data available.</div>;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App, { splitArray, properName, convertMonth, convertMonths } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test(`gets store name only from raw string`, () => {
+//   expect(getStore(`best_buy_202101_percent`, /\d{6}/)).toMatch(/best_buy/);
+// });
+
+test(`turns raw string into 2D array`, () => {
+  const array = [
+    `brand`,
+    `best_buy_202101`,
+    `best_buy_202102`,
+    `best_buy_202103`,
+    `lowes_202101`,
+    `lowes_202102`,
+    `lowes_202103`,
+  ];
+  expect(splitArray(array)[1]).toHaveProperty(`name`, `best_buy`);
+  expect(splitArray(array)[1]).toHaveProperty(
+    `months`,
+    expect.arrayContaining([`202101`])
+  );
+});
+
+test(`capitalizes every word in phrase`, () => {
+  expect(properName(`best_buy`)).toMatch(/Best Buy/);
+});
+
+test(`converts raw number into 3-letter month abbreviation`, () => {
+  expect(convertMonth(`202102`)).toMatch(`Feb`);
+});
+
+test(`converts array of raw numbers into 3-letter month abbreviations`, () => {
+  expect(convertMonths([`202101`, `202102`, `202103`])).toEqual([`Jan`, `Feb`, `Mar`]);
 });
